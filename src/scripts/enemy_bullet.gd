53061538eb86cb2ebfc97c8c26885031188f5c48
Martin Cerdeira <martincerdeira@gmail.com>
2022-07-05 16:19:29 -0300

    Enemy particles

--- src/scripts/enemy_bullet.gd
+++ src/scripts/enemy_bullet.gd
@@ -1,5 +1,6 @@
 extends Area2D
 var speed = 100
+var particle = preload("res://scenes/particle.tscn")
 
 func initialize(rot):
 	look_at(rot)
@@ -15,4 +16,11 @@ func _on_visinot_screen_exited():
 func _on_enemy_bullet_area_entered(area):
 	if area.name == "player":
 		area.hurt()
+		queue_free()
+
+func _on_enemy_bullet_body_entered(body):
+	if body.name == "TileMap":
+		var w = particle.instance()
+		get_parent().add_child(w)
+		w.set_position(position)
 		queue_free()
